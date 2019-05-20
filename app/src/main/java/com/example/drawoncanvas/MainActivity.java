4e90a908c59689351c1034ab7a3985dc7678e852
RanaHossam <ranahossam263@gmail.com>
2019-05-20 12:05:19 +0200

    camera permission in main activity

--- app/src/main/java/com/example/drawoncanvas/MainActivity.java
+++ app/src/main/java/com/example/drawoncanvas/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.drawoncanvas;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.annotation.TargetApi;
@@ -17,12 +23,15 @@ import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+//import android.R;
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
     SurfaceView cameraView, transparentView;
     SurfaceHolder holder, holderTransparent;
     Camera camera;
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
     private float RectLeft, RectTop, RectRight, RectBottom;
     int deviceHeight, deviceWidth;
@@ -31,20 +40,28 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cameraView = (SurfaceView) findViewById(R.id.CameraView);
-        holder = cameraView.getHolder();
-        holder.addCallback((SurfaceHolder.Callback) this);
-        //holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        cameraView.setSecure(true);
-        // Create second surface with another holder (holderTransparent)
-        transparentView = (SurfaceView) findViewById(R.id.TransparentView);
-        holderTransparent = transparentView.getHolder();
-        holderTransparent.addCallback((SurfaceHolder.Callback) this);
-        holderTransparent.setFormat(PixelFormat.TRANSLUCENT);
-        transparentView.setZOrderMediaOverlay(true);
-        //getting the device heigth and width
-        deviceWidth = getScreenWidth();
-        deviceHeight = getScreenHeight();
+
+
+        if (checkPermission()) {
+            cameraView = (SurfaceView) findViewById(R.id.CameraView);
+            holder = cameraView.getHolder();
+            holder.addCallback((SurfaceHolder.Callback) this);
+            //holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+            cameraView.setSecure(true);
+            // Create second surface with another holder (holderTransparent)
+            transparentView = (SurfaceView) findViewById(R.id.TransparentView);
+            holderTransparent = transparentView.getHolder();
+            holderTransparent.addCallback((SurfaceHolder.Callback) this);
+            holderTransparent.setFormat(PixelFormat.TRANSLUCENT);
+            transparentView.setZOrderMediaOverlay(true);
+            //getting the device heigth and width
+            deviceWidth = getScreenWidth();
+            deviceHeight = getScreenHeight();
+
+        } else {
+            requestPermission();
+        }
+
     }
 
     public static int getScreenWidth() {
@@ -54,6 +71,58 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
     public static int getScreenHeight() {
         return Resources.getSystem().getDisplayMetrics().heightPixels;
+    }
+    private boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
+        }
+        return true;
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+
+                    // main logic
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                                != PackageManager.PERMISSION_GRANTED) {
+                            showMessageOKCancel("You need to allow access permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermission();
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                }
+                break;
+        }
+    }
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
     }
 
     private void Draw() {
